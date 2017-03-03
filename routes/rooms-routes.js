4a79ebbce4502535888785a24805650c28bd2f84
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-03-03 09:27:08 -0500

    Add file uploads

--- routes/rooms-routes.js
+++ routes/rooms-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const ensure = require('connect-ensure-login');
+const multer = require('multer');
 
 const Room = require('../models/room-model.js');
 
 const roomsRoutes = express.Router();
+const multerThing = multer({ dest: __dirname + '/../public/uploads/' });
 
 
 roomsRoutes.get('/my-rooms', ensure.ensureLoggedIn(), (req, res, next) => {
@@ -23,24 +25,35 @@ roomsRoutes.get('/rooms/new', ensure.ensureLoggedIn(), (req, res, next) => {
   });
 });
 
-roomsRoutes.post('/rooms', ensure.ensureLoggedIn(), (req, res, next) => {
-  const newRoom = new Room ({
-    name:  req.body.name,
-    desc:  req.body.desc,
-    picture: 'https://media.giphy.com/media/d2ZjBlsQa5dWO45a/giphy.gif',
-    owner: req.user._id   // <-- we add the user ID
-  });
+roomsRoutes.post('/rooms',
+  ensure.ensureLoggedIn(),
+    // <input id="picture-input" type="file" name="picture">
+    //                                                |
+    //                   ------------------------------
+    //                   |
+  multerThing.single('picture'),
 
-  newRoom.save((err) => {
-    if (err) {
-      next(err);
-      return;
-    } else {
-      req.flash('success', 'Your room has been created.');
-      res.redirect('/rooms/new');
-    }
-  });
-});
+  (req, res, next) => {
+    const filename = req.file.filename;
+
+    const newRoom = new Room ({
+      name:  req.body.name,
+      desc:  req.body.desc,
+      picture: `/uploads/${filename}`,
+      owner: req.user._id   // <-- we add the user ID
+    });
+
+    newRoom.save((err) => {
+      if (err) {
+        next(err);
+        return;
+      } else {
+        req.flash('success', 'Your room has been created.');
+        res.redirect('/rooms/new');
+      }
+    });
+  }
+);
 
 
 module.exports = roomsRoutes;
